0a5404f9fa69ec8574d2e1e9455ddc00511f0b71
Nika Gabeskiria <nikoloz.gabeskiria@gmail.com>
2024-10-25 12:24:14 +0400

    initial commit

---
+++ note/models.py
@@ -0,0 +1,29 @@
+
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import slugify
+
+class Note(models.Model):
+
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    slug = models.SlugField(null=False, unique=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Note"
+        verbose_name_plural = "Notes"
+        ordering = ["-id"]
+
+    def __str__(self):
+        return self.title
+    
+    
+    def save(self, *args,**kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
+
+# es file 35:56 wuTze daamtavra 
+
+
